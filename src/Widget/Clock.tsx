1fc05a61190d96b1837b16cdfdd7bc94125cf550
kelvinko <kelvinkoko@gmail.com>
2024-03-20 15:56:54 +0900

    Add clock hands

--- src/Widget/Clock.tsx
+++ src/Widget/Clock.tsx
@@ -6,6 +6,8 @@ import styles from "./Clock.module.scss";
 
 const Clock = () => {
   const [time, setTime] = useState<string>("");
+  const [hourAngle, setHourAngle] = useState<number>(0);
+  const [minuteAngle, setMinuteAngle] = useState<number>(0);
 
   useEffect(() => {
     const updateTime = () => {
@@ -17,6 +19,9 @@ const Clock = () => {
       // Create a blinking effect for the semi-colon
       const separator = seconds % 2 === 0 ? ":" : " ";
 
+      const minutesPassed = (date.getHours() % 12) * 60 + date.getMinutes();
+      setHourAngle((minutesPassed * 360) / (12 * 60));
+      setMinuteAngle((date.getMinutes() * 360) / 59);
       setTime(`${hours}${separator}${minutes}`);
     };
 
@@ -35,19 +40,31 @@ const Clock = () => {
         <div className={styles.clockFace}>
           {_.range(1, 13).map(i => {
             const radian = ((i * 30 + 180) * Math.PI) / 180;
-            const x = -(radius * Math.sin(radian)) + radius;
-            const y = radius * Math.cos(radian) + radius;
+            const x = -((radius - 9) * Math.sin(radian)) + radius;
+            const y = (radius - 9) * Math.cos(radian) + radius;
             return (
               <div
                 className={styles.hourMark}
                 style={{
-                  transform: `translateX(${x}px) translateY(${y}px)`
+                  transform: `translateX(${x - 9}px) translateY(${y - 9}px)`
                 }}
               >
                 {i}
               </div>
             );
           })}
+          <div
+            className={styles.hourHand}
+            style={{
+              transform: `rotate(${hourAngle + 180}deg)`
+            }}
+          />
+          <div
+            className={styles.minuteHand}
+            style={{
+              transform: `rotate(${minuteAngle + 180}deg)`
+            }}
+          />
         </div>
       </div>
     </Dome>
